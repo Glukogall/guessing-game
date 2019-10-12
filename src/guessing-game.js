e4b09b31146e6a1c81b4b9d6432c813c8dc36fd0
Galka <gallkogluk@gmail.com>
2019-10-13 01:04:39 +0400

    feat: add answer for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.less = min;
+        this.more = max;
     }
 
     guess() {
-
+        return this.average=Math.round((this.less+this.more)/2)
     }
 
     lower() {
-
+        this.more=this.average
     }
-
+ 
     greater() {
-
+        this.less=this.average
     }
 }
 
